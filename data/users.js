5fdc59be7032dbe10e8f45b1e5c531667d46fe0c
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-03-28 06:44:41 -0700

    created ejs views template

---
+++ data/users.js
@@ -0,0 +1,29 @@
+// The "users" data will be information about
+// the application's user base.
+ const users = [
+	{  id: 1,
+		name: "Tyrone Bradford",
+		phone: "(328) 473-5914",
+		country: "Vietnam"
+	},
+	{  id: 2,
+		name: "Sigourney Craig",
+		phone: "1-557-521-3808",
+		country: "India"
+	},
+	{  id: 3,
+		name: "Ginger Graham",
+		phone: "1-377-929-0816",
+		country: "Italy"
+	},
+	{  id: 4,
+		name: "Lamar Conrad",
+		phone: "(434) 667-2008",
+		country: "Gambia"
+	},
+	{  id: 5,
+		name: "Shaeleigh Charles",
+		phone: "1-653-283-6862",
+		country: "Senegal"
+	}
+]
--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 console.log("Nakamu");
 const express = require("express");
 //creating an express application
-const app = express(); 
+const app = express();
 //PORT CONSTANT
 const PORT = 8080;
+// app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: true }));
+// // for json code
+// app.use(express.json());
+
+app.set("view engine", "ejs");
 
 
+app.get("/", (req, res) => {
+  res.render("index")
+  
 
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port: ${PORT}`);
-  }) 
+});
+
+// app.use(logger)
+
+//linking up my user routes
+// const userRouter = require("./routes/users");
+
+// app.use("/users", userRouter);
+
+//midleware function
+// function logger(req , res, next) {
+//     console.log(req.originalUrl)
+//     next()
+// }
+
+
+// app.post("/region", (req, res) => {
+//   res.render("users");
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
